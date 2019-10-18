3ad7fe936ca2680b2c71eb84db9bfb3befd31183
Joe Vogel <joejvogel@gmail.com>
2019-10-18 15:43:35 -0300

    Movido para aws

--- server.js
+++ server.js
@@ -13,20 +13,33 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
+let transporter = nodemailer.createTransport({
+    host: "smtpout.secureserver.net",
+    secure: true,
+    port: 465,
+    auth: {
+        user: process.env.USER_MAIL,
+        pass: process.env.MAIL_PASS
+    },
+    tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false
+    }
+})
+
+transporter.verify(function (error, success) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Server is ready to take our messages");
+    }
+})
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
 app.post('/api/contact', function (req, res) {
-
-    let transporter = nodemailer.createTransport({
-        service: 'Godaddy',
-        secureConnection: false,
-        auth: {
-            user: process.env.USER_MAIL,
-            pass: process.env.MAIL_PASS
-        }
-    }) //ou porta 587
 
     // host: "smtpout.secureserver.net",
     // port: 587,
