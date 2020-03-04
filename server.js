485186005433f28053a0feef79dfb4edb90e8385
Joe Vogel <joejvogel@gmail.com>
2020-03-04 11:25:43 -0300

    Nova versão do site. SU Tech

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 const express = require('express')
-const nodemailer = require('nodemailer')
 const bodyParser = require('body-parser')
 
 let app = express()
@@ -13,50 +12,8 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
-let transporter = nodemailer.createTransport({
-    host: "smtpout.secureserver.net",
-    secure: true,
-    port: 465,
-    auth: {
-        user: process.env.USER_MAIL,
-        pass: process.env.MAIL_PASS
-    }
-})
-
-transporter.verify(function (error, success) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Server is ready to take our messages");
-    }
-})
-
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html")
-})
-
-app.post('/api/contact', function (req, res) {
-
-    let mailOptions = {
-        from: 'no_reply@sementeurbana.com',
-        to: 'contato@sementeurbana.com',
-        subject: 'Semente Urbana - Novo contato no SITE - ' + req.body.name,
-        html: '<body><h5>Um novo contato foi feito via site!<h5> <p><strong>Dados do contato:</strong></p><p>Nome: ' + req.body.name + '<br>Telefone: ' + req.body.phone + '<br>E-mail: ' + req.body.email + '</p></body>'
-    }
-
-    transporter.sendMail(mailOptions, (error, info) => {
-
-        if (error) {
-            console.log(error)
-            res.status(500).send('Error sending e-mail')
-            return
-        }
-
-        console.log('Message %s sent: %s', info.messageId, info.response)
-        res.status(200).send('Success')
-
-    })
-
 })
 
 app.listen(port, function () {
